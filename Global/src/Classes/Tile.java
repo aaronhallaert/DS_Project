8c3f05fb2b7a800dbf3af38c580ec0c0888fc4f4
Tibotje <r0620211@kuleuven.be>
2018-11-16 15:38:28 +0100

    Global/Classes commentaar is klaar

--- Global/src/Classes/Tile.java
+++ Global/src/Classes/Tile.java
@@ -4,32 +4,42 @@ import javafx.scene.layout.StackPane;
 
 import java.io.Serializable;
 
+/**
+ * deze klasse dient om een tile voor te stellen in de APPSERVER, dus niet in de CLIENT
+ * deze klasse bevat al het nodige om een VisualTile object in te laden
+ * de voornamelijke verschillen zitten hem in :
+ *          - VisualTile bevat animaties om om te draaien
+ *          - VisualTile bevat de byte[] voorkantFoto en achterkantFoto, terwijl Tile enkel de URL of naam bevat om de byte[] te
+ *              krijgen uit de database
+ *
+ *
+ * Dus :    Tile = bevat al het nodige in de appserver om een echte VisualTile te maken in de client
+ *
+ */
 public class Tile implements Serializable {
 
-    //tile = de waarden van de tegel -> niet de voorsetlling, does not extends stackpane dus
-    //tegelVisueel = de visuele voorstelling van de tegel.
+    private int uniqueIdentifier; // elke tile van een spelletje heeft een verschillende uniqueIdentifier
+    private int id;               // in 1 spelletje zijn er telkens 2 tiles met dezelfde id -> gebruikt om te vergelijken als dezelfde
+    private String imageId;       // onder welke naam de voorkant afbeelding in de databank zit
+    private String backImageId;   // idem maar voor achterkant
 
-    private int uniqueIdentifier;
-    private int id;
-    private String imageId;
-    private String backImageId;
+    //extras
+    private boolean found;        // eenmaal gevonden -> true, anders -> false
+    private boolean flippedOver;  // als het kaartje eens gedraaid is
 
-    //extras?
-    private boolean found;
-    private boolean flippedOver;
 
-    //bevat de afbeelding niet!, bevat wel de naam van de afbeelding mss best
     public Tile(int uniqueIdentifier, int id, String imageId, String backImageId){
         this.uniqueIdentifier = uniqueIdentifier;
         this.id = id;
         this.imageId = imageId;
         this.backImageId = backImageId;
 
-        //extras?
+        //extras
         found = false;
         flippedOver= false;
     }
 
+    //getters en setters
     public int getUniqueIdentifier() {
         return uniqueIdentifier;
     }
